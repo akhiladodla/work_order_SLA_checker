2a7b160085ec464912dd33b9d3c9c362acb44f12
Akhila Dodla <akhilareddydodla542@gmail.com>
2026-03-13 17:12:25 +0530

    Update server_log_error_counter.py

--- server_log_error_counter.py
+++ server_log_error_counter.py
@@ -9,3 +9,5 @@ def count_log_levels(logs):
     return counts
 logs=["ERROR:disk full","INFO:started","ERROR:timeout","WARN:low memory"]
 print(count_log_levels( logs))
+//the main goal of the code is to run loop through each log and extract how many times it is showing the messages or logs like full,started,timeout and low memory
+//then after running the entire loop it prints the count of log occured.
