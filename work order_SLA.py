d64d6a3109a897dc67443ece6763f880cdc0c137
Akhila Dodla <akhilareddydodla542@gmail.com>
2026-03-13 17:16:41 +0530

    Update work order_SLA.py

--- work order_SLA.py
+++ work order_SLA.py
@@ -5,3 +5,6 @@ print(get_sla_hours("p1"))
 print(get_sla_hours("p2"))
 print(get_sla_hours("p3"))
 print(get_sla_hours("p4"))
+//Here,we need to print the priorities by mentioning p1,p2,p3,p4
+//p1,p2,p3,p4 referes the critical,high,medium,low as mentioned in the statement
+//it returns the values like 4,8,24,72 if it present in the range,else returns-1
